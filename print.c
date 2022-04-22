048554ad0569ea2af71fac1d9c9b8bb2a07f32db
yelosolutions <yelosolutions69@gmail.com>
2022-04-22 22:55:06 +0000

    My second commit

--- print.c
+++ print.c
@@ -17,12 +17,12 @@ void cleanup(va_list args, buffer_t *output)
 }
 
 /**
- * run_printf - Reads through the format string for _printf.
- * @format: Character string to print - may contain directives.
- * @output: A buffer_t struct containing a buffer.
- * @args: A va_list of arguments.
+ * run_printf - Reads through the format string for _printf
+ * @format: Character string to print,contain directives
+ * @output: A buffer_t struct containing a buffer
+ * @args: A va_list of arguments
  *
- * Return: The number of characters stored to output.
+ * Return: The number of characters stored to output
  */
 int run_printf(const char *format, va_list args, buffer_t *output)
 {
